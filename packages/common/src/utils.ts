db6b5fe32b51fff1bc4c5d9b02b7db0af60581e7
pabra <patrick+github-commit-2020@braune.xyz>
2020-06-05 15:15:41 +0200

    use proper type to extend from

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -1,9 +1,6 @@
-export const isKeyof = <T extends {}>(
-  obj: T,
-  key: string | number | symbol,
-): key is keyof T => Object.prototype.hasOwnProperty.call(obj, key);
+type Key = string | number | symbol;
 
-// export const isStringKeyof = <T extends Record<string, unknown>>(
-//   obj: T,
-//   key: string|number|symbol,
-// ): key is keyof T => isKeyof(obj, key);
+export const isKeyof = <T extends Record<Key, any>>(
+  obj: T,
+  key: Key,
+): key is keyof T => Object.prototype.hasOwnProperty.call(obj, key);
