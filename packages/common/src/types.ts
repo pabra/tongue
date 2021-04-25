0c4238f0c97f11dfff87b5772fd89e1beee38758
pabra <patrick+github-commit-2021@braune.xyz>
2021-04-25 09:07:06 +0200

    use updated runtypes

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,29 +1,28 @@
 import {
-  null as srNull,
+  dictionary as srDictionary,
+  nullOr as srNullOr,
+  optional as srOptional,
   partial as srPartial,
   record as srRecord,
   string as srString,
-  stringIndex as srStringIndex,
-  union as srUnion,
 } from 'simple-runtypes';
 
 export const validArgExp = /[a-zA-Z][a-zA-Z0-9_]*/;
 export const fullArgExp = new RegExp('^' + validArgExp.source + '$');
 
-export const entriesFile = srStringIndex(
-  srPartial(
-    srRecord({
-      hint: srString(),
-      args: srStringIndex(srString()),
-    }),
-  ),
+export const entriesFile = srDictionary(
+  srString(),
+  srRecord({
+    hint: srOptional(srString()),
+    args: srOptional(srDictionary(srString(), srString())),
+  }),
 );
 
-export const translationFile = srStringIndex(srUnion(srNull(), srString()));
+export const translationFile = srDictionary(srString(), srNullOr(srString()));
 
 const config = srRecord({
   entriesFile: srString(),
-  translationsFiles: srStringIndex(srString()),
+  translationsFiles: srDictionary(srString(), srString()),
   srcDir: srString(),
 });
 export const configFile = srPartial(config);
